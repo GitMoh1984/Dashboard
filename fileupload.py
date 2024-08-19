f1ee96c9b8b315e9365967b274ee94c6b01b303e
GitMoh1984 <md_alla21@yahoo.co.in>
2024-08-19 07:55:01 -0400

    Create fileupload.py
    created fileupload.py as part for dashboard changes

---
+++ fileupload.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import streamlit as st
+import cx_Oracle
+import pandas
+import os
+on = st.toggle("Show Pending Status ")
+st.sidebar.markdown("# Main page 🎈")
+upload_file = st.file_uploader("Choose a CSV or file", type="csv")
+if upload_file is not None:
+    df = pd.read_csv(upload_file)
+    st.subheader("Data Preview")
+    st.write(df.head())
+    st.subheader("Data Summary")
+    st.write(df.describe())
+    st.subheader("Filter data")
+    columns = df.columns.tolist()
+    selected_column = st.selectbox("Select Columns to Filter ", columns)
+    unique_values = df[selected_column].unique()
+    selected_value = st.selectbox("Select Values to be Filtered", unique_values)
+    filtered_df = df[df[selected_column] == selected_value]
+    st.subheader("Display Filtered Data")
+    st.write(filtered_df)
