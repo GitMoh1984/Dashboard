e22d5b902c37eb7bf958541cb3c51e4a9d5c90e0
GitMoh1984 <md_alla21@yahoo.co.in>
2024-08-19 07:51:26 -0400

    Update DMTDashboard_app.py
    DMTupdate-8/19 7:51 AM EST

--- DMTDashboard_app.py
+++ DMTDashboard_app.py
@@ -1,21 +1,7 @@
 import streamlit as st
-import pandas as pd
 
-st.title("welcome to DMT Dashboard")
-st.sidebar.markdown("# Main page 🎈")
-upload_file = st.file_uploader("Choose a CSV or file", type="csv")
-if upload_file is not None:
-    df = pd.read_csv(upload_file)
-    st.subheader("Data Preview")
-    st.write(df.head())
-    st.subheader("Data Summary")
-    st.write(df.describe())
-    st.subheader("Filter data")
-    columns = df.columns.tolist()
-    selected_column = st.selectbox("Select Columns to Filter ", columns)
-    unique_values = df[selected_column].unique()
-    selected_value = st.selectbox("Select Values to be Filtered", unique_values)
-    filtered_df = df[df[selected_column] == selected_value]
-    st.subheader("Display Filtered Data")
-    st.write(filtered_df)
-st.sidebar.markdown("# Sub page ")
+fileupload_pg = st.Page("fileupload.py", title="File Upload", icon=":material/upload:")
+event_status_pg = st.Page("event_status.py", title="event_status", icon=":material/update:")
+pg = st.navigation([fileupload_pg, event_status_pg ])
+st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+pg.run()
